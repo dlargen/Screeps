5ff1a81332618443e755b9b08a0f1ff1dea2b0f3
Dave Largen <dave.largen@gmail.com>
2020-09-10 11:09:39 -0400

    Working Tower Code + spawn tweeks.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var harvesterQty = 4;
-var builderQty = 4;
-var upgraderQty = 8;
+var builderQty = 5;
+var upgraderQty = 5;
 
 
 module.exports.loop = function () {
@@ -15,66 +15,71 @@ module.exports.loop = function () {
         }
     }
     
-    //console.log(Game.spawns['Spawn1'].room.controller.level);
+    // *** SPAWNING EXTENSIONS *** //
     if (Game.spawns['Spawn1'].room.controller.level > 1) {
         var extensions = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
             filter: { structureType: STRUCTURE_EXTENSION }
         });
         
-        console.log('Spawn has '+extensions.length+' extensions.');
+        //console.log('Spawn has '+extensions.length+' extensions.');
         
         var constructionExtensions = Game.spawns['Spawn1'].room.find(FIND_MY_CONSTRUCTION_SITES, {
             filter: { structureType: STRUCTURE_EXTENSION }
         });
         
-        console.log('Spawn has '+constructionExtensions.length+' construction extensions.');
+        //console.log('Spawn has '+constructionExtensions.length+' construction extensions.');
         
         if (extensions.length < 1 && constructionExtensions.length < 1) {
-            console.log('Spawning extension 1');
+            //console.log('Spawning extension 1');
             var spawnPosition = Game.spawns['Spawn1'].pos;
             //console.log('New Position '+ spawnPosition);
             Game.spawns['Spawn1'].room.createConstructionSite(spawnPosition.x - 2 , spawnPosition.y + 2, STRUCTURE_EXTENSION);
             
-            console.log('Spawning extension 2');
+            //console.log('Spawning extension 2');
             var spawnPosition = Game.spawns['Spawn1'].pos;
             //console.log('New Position '+ spawnPosition);
             Game.spawns['Spawn1'].room.createConstructionSite(spawnPosition.x , spawnPosition.y + 2, STRUCTURE_EXTENSION);
         }
     }
-    /*
-    
-    var towers = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
-        filter: { structureType: STRUCTURE_TOWER }
-    });
-    //console.log('Spawn has '+towers.length+' towers');
-    
-    if (towers.length < 1) {
-        //console.log('Spawn has '+towers.length+' towers');
-        var spawnPosition = Game.spawns['Spawn1'].pos;
-        spawnPosition.x -= 4;
-        //console.log('New Position '+ spawnPosition);
-        Game.spawns['Spawn1'].room.createConstructionSite(spawnPosition.x, spawnPosition.y, STRUCTURE_TOWER);
-    }
-    
-    for (var id in towers) {
-        if (id >= 0) {
-            //console.log('tower id ' +id);
-            var tower = towers[id];
-            //console.log('tower pos ' +tower.pos);
-            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if(closestHostile) {
-                tower.attack(closestHostile);
-            }
-            
-            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if(closestDamagedStructure) {
-                tower.repair(closestDamagedStructure);
+
+    // *** SPAWNING TOWERS *** //
+    if (Game.spawns['Spawn1'].room.controller.level > 2) {
+        var towers = Game.spawns['Spawn1'].room.find(FIND_MY_STRUCTURES, {
+            filter: { structureType: STRUCTURE_TOWER }
+        });
+        
+        //console.log('Spawn has '+towers.length+' towers.');
+        
+        var constructionTowers = Game.spawns['Spawn1'].room.find(FIND_MY_CONSTRUCTION_SITES, {
+            filter: { structureType: STRUCTURE_TOWER }
+        });
+        
+        //console.log('Spawn has '+constructionTowers.length+' construction towers.');
+        
+        if (towers.length < 1 && constructionTowers.length < 1) {
+            console.log('Spawning Tower 1');
+            var spawnPosition = Game.spawns['Spawn1'].pos;
+            Game.spawns['Spawn1'].room.createConstructionSite(spawnPosition.x - 4 , spawnPosition.y, STRUCTURE_TOWER);
+        }
+        for (var id in towers) {
+            if (id >= 0) {
+                //console.log('tower id ' +id);
+                var tower = towers[id];
+                //console.log('tower pos ' +tower.pos);
+                var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(closestHostile) {
+                    tower.attack(closestHostile);
+                }
+                
+                var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+                });
+                if(closestDamagedStructure) {
+                    tower.repair(closestDamagedStructure);
+                }
             }
         }
     }
-    */
     
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     //console.log('Harvesters: ' + harvesters.length);
@@ -89,7 +94,7 @@ module.exports.loop = function () {
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     //console.log('Upgraders: ' + harvesters.length);
 
-    if(upgraders.length < upgraderQty && harvesters.length == harvesterQty) {
+    if(upgraders.length < upgraderQty && harvesters.length >= harvesterQty) {
         var newName = 'Upgrader' + Game.time;
         //console.log('Spawning new upgrader: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, 
@@ -99,7 +104,7 @@ module.exports.loop = function () {
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     //console.log('Builders: ' + builders.length);
 
-    if(builders.length < builderQty && harvesters.length == harvesterQty & upgraders.length == upgraderQty) {
+    if(builders.length < builderQty && harvesters.length >= harvesterQty & upgraders.length >= upgraderQty) {
         var newName = 'Builder' + Game.time;
         //console.log('Spawning new builder: ' + newName);
         Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE,MOVE], newName, 
--- role.harvester.js
+++ role.harvester.js
@@ -13,7 +13,8 @@ var roleHarvester = {
             var targets = creep.room.find(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION || 
-                            structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.structureType == STRUCTURE_SPAWN ||
+                            structure.structureType == STRUCTURE_TOWER) &&
                             structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                     }
             });
