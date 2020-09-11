c597441109b94feef031f686921044b97c44d1df
Dave Largen <dave.largen@gmail.com>
2020-09-10 20:23:23 -0400

    Upgraders pull from closest storage first.

--- role.upgrader.js
+++ role.upgrader.js
@@ -12,10 +12,24 @@ var roleUpgrader = {
 	    }
 	    
 	    if(!creep.memory.upgrading) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+	        
+	        var container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+	            filter: (structure) => {
+		        return structure.structureType == STRUCTURE_CONTAINER && 
+			    structure.store[RESOURCE_ENERGY] > 500}});
+			
+			var returnValue = creep.withdraw(container,RESOURCE_ENERGY);
+			
+			if (returnValue == ERR_NOT_IN_RANGE)
+			    creep.moveTo(container);
+			else if(returnValue < 0)
+			{
+                var source = creep.pos.findClosestByRange(FIND_SOURCES_ACTIVE);
+                
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+			}
         }
         else {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
